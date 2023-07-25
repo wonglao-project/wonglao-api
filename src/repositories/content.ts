6fa4f6737cd27b52a8d4f012ca7028f86e55f87e
Sujaree Inthanarm <ppanng.s@pang-3.local>
2023-07-25 16:56:53 +0700

    repo updatecontent

--- src/repositories/content.ts
+++ src/repositories/content.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { ICreateContent, IContent } from "../entities/content";
+import { ICreateContent, IContent, IUpdate } from "../entities/content";
 import { IRepositoryContent } from ".";
 
 export function newRepositoryContent(db: PrismaClient): IRepositoryContent {
@@ -72,24 +72,53 @@ class RepositoryContent implements IRepositoryContent {
       where: { id },
     });
   }
-  // private convertToPrismaCategory(sc: SellerCategory): PrismaSellerCategory {
-  //   switch (sc) {
-  //     case SellerCategory.Bar:
-  //       return "Bar";
-  //     case SellerCategory.Brewer:
-  //       return "Brewer";
-  //   }
-  // }
-  // private convertToPrismaProductCategory(
-  //   pc: ProductCategory
-  // ): PrismaProductCategory {
-  //   switch (pc) {
-  //     case ProductCategory.Gin:
-  //       return "Gin";
-  //     case ProductCategory.Rum:
-  //       return "Rum";
-  //     case ProductCategory.WhiteSpirit:
-  //       return "WhiteSpirit";
-  //   }
-  // }
+
+  async updateUserContent(arg: IUpdate): Promise<IContent> {
+    const uSeller = await this.db.seller.findUnique({
+      where: { id: arg.id },
+    });
+
+    if (!uSeller) {
+      return Promise.reject(`no such content ${arg.id}`);
+    }
+
+    if (uSeller.userId !== arg.userId) {
+      return Promise.reject(`bad ownerId : ${arg.userId}`);
+    }
+
+    return await this.db.seller.update({
+      where: { id: arg.id },
+      data: {
+        userId: arg.userId,
+        operating_time: arg.operating_time,
+        description: arg.description,
+        address: arg.address,
+        tel: arg.tel,
+        email: arg.email,
+        category: arg.category,
+        product_category: arg.product_category,
+        images: arg.images,
+      },
+    });
+  }
 }
+// private convertToPrismaCategory(sc: SellerCategory): PrismaSellerCategory {
+//   switch (sc) {
+//     case SellerCategory.Bar:
+//       return "Bar";
+//     case SellerCategory.Brewer:
+//       return "Brewer";
+//   }
+// }
+// private convertToPrismaProductCategory(
+//   pc: ProductCategory
+// ): PrismaProductCategory {
+//   switch (pc) {
+//     case ProductCategory.Gin:
+//       return "Gin";
+//     case ProductCategory.Rum:
+//       return "Rum";
+//     case ProductCategory.WhiteSpirit:
+//       return "WhiteSpirit";
+//   }
+// }
