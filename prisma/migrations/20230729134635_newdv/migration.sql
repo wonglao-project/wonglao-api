29d6c44aed434cd4214c3733bff58c24d5d4a2e6
Sujaree Inthanarm <ppanng.s@pang-3.local>
2023-07-29 20:51:12 +0700

    fix schema.prisma

---
+++ prisma/migrations/20230729134635_newdv/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - Added the required column `userId` to the `Product` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Product" ADD COLUMN     "images" TEXT[],
+ADD COLUMN     "userId" TEXT NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
